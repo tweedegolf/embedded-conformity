979d99f25be94faa45e27a7b95a8818333e773d2
Vivian <vivian@0x76.dev>
2025-04-23 14:46:41 +0200

    more hello world

---
+++ client/README.md
@@ -0,0 +1,3 @@
+# Client
+
+This is the client device in the test suite it will simulate various signals to the device under test to ensure it is working
---
+++ runner/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // 1. Upload firmware to DUT
+    // 2. Upload firmware to client (RP2040)
+    // 3. Start Tests
+    // 4. Report Status
+}
