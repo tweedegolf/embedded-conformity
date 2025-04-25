3c9a815675fff3ef92da90e0e3c2cb94ad3519f7
Vivian <vivian@0x76.dev>
2025-04-25 11:13:52 +0200

    probe_rs list

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,4 +1,12 @@
+use probe_rs::probe::{list::Lister, Probe};
+use probe_rs::Permissions;
+
 fn main() {
+    let lister = Lister::new();
+    let probes = lister.list_all();
+
+    dbg!(probes);
+
     // 1. Upload firmware to DUT
     // 2. Upload firmware to client (RP2040)
     // 3. Start Tests
