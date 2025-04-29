9198da839014ed7fb77f99a77161b3fd55db259e
Vivian <vivian@0x76.dev>
2025-04-29 11:30:18 +0200

    two-way RTT

--- fake-peripheral/src/main.rs
+++ fake-peripheral/src/main.rs
@@ -5,14 +5,16 @@
 use core::time::Duration;
 
 use embassy_executor::Spawner;
-use embassy_rp::bind_interrupts;
 use embassy_rp::gpio::{Level, Output};
 use embassy_rp::peripherals::PIO0;
 use embassy_rp::pio::{InterruptHandler, Pio};
 use embassy_rp::pio_programs::pwm::{PioPwm, PioPwmProgram};
+use embassy_rp::{Peripherals, bind_interrupts};
 use embassy_time::Timer;
-use rtt_target::{rprintln, rtt_init_defmt, rtt_init_print};
 use panic_probe as _;
+use rtt_target::{
+    ChannelMode, DownChannel, rprintln, rtt_init, rtt_init_defmt, rtt_init_print, set_print_channel,
+};
 
 use defmt::info;
 
@@ -24,31 +26,64 @@ bind_interrupts!(struct Irqs {
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    // rtt_init_defmt!();
-    rtt_init_print!();
+    let mut channels = rtt_init! {
+        up: {
+            0: {
+                size: 1024,
+                mode: ChannelMode::NoBlockSkip,
+                name: "Log"
+            }
+        }
+        down: {
+            0: {
+                size: 1024,
+                name: "Control"
+            }
+        }
+    };
+    set_print_channel(channels.up.0);
 
     let p = embassy_rp::init(Default::default());
-    let Pio { mut common, sm0, .. } = Pio::new(p.PIO0, Irqs);
 
     let mut led = Output::new(p.PIN_13, Level::Low);
     led.set_high();
 
+    wait_for_host(&mut channels.down.0);
+
     loop {
-        rprintln!("Hello World");
         Timer::after_millis(100).await;
         led.toggle();
     }
+}
 
-    // // Note that PIN_25 is the led pin on the Pico
-    // let prg = PioPwmProgram::new(&mut common);
-    // let mut pwm_pio = PioPwm::new(&mut common, sm0, p.PIN_13, &prg);
-    // pwm_pio.set_period(Duration::from_micros(REFRESH_INTERVAL));
-    // pwm_pio.start();
-    //
-    // let mut duration = 0;
-    // loop {
-    //     duration = (duration + 1) % 1000;
-    //     pwm_pio.write(Duration::from_micros(duration));
-    //     Timer::after_millis(1).await;
-    // }
+fn wait_for_host(down: &mut DownChannel) {
+    let mut read_buf = [0; 1024];
+    let mut read;
+    'outer: loop {
+        read = down.read(&mut read_buf);
+        for i in 0..read {
+            if read_buf[i] == 42 {
+                break 'outer;
+            }
+        }
+    }
+    rprintln!("Starting test");
+}
+
+async fn pio_example(p: Peripherals) {
+    let Pio {
+        mut common, sm0, ..
+    } = Pio::new(p.PIO0, Irqs);
+
+    let prg = PioPwmProgram::new(&mut common);
+    let mut pwm_pio = PioPwm::new(&mut common, sm0, p.PIN_13, &prg);
+    pwm_pio.set_period(Duration::from_micros(REFRESH_INTERVAL));
+    pwm_pio.start();
+
+    let mut duration = 0;
+    loop {
+        duration = (duration + 1) % 1000;
+        pwm_pio.write(Duration::from_micros(duration));
+        Timer::after_millis(1).await;
+    }
 }
--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,56 +1,64 @@
 use std::path::Path;
+use std::process::Command;
 use std::thread::sleep;
 use std::time::Duration;
 
 use probe_rs::Permissions;
-use probe_rs::flashing::{
-    DownloadOptions, FlashProgress, Format, download_file, download_file_with_options,
-};
-use probe_rs::probe::{Probe, list::Lister};
+use probe_rs::config::TargetSelector;
+use probe_rs::flashing::{Format, download_file};
+use probe_rs::probe::DebugProbeInfo;
+use probe_rs::probe::list::Lister;
 use probe_rs::rtt::Rtt;
-use probe_rs::rtt::ScanRegion::Ram;
 
 // TODO: Use clap to take these as arguments
 const DEBUG_PROBE_UUID: &str = "E6614103E78B5024";
 const FAKE_PERIPHERAL_FIRMWARE_PATH: &str =
     "../fake-peripheral/target/thumbv6m-none-eabi/release/fake-peripheral";
 
+// 1. Upload firmware to DUT: https://probe.rs/docs/library/quickstart/#downloading-to-flash
+// 2. Upload firmware to client (RP2040)
+// 3. Start Tests, https://docs.rs/rtt-target/latest/rtt_target/#reading
+// 4. Report Status, use defmt and rtt to read back from the chips?
+
 fn main() {
     let lister = Lister::new();
     let probes = lister.list_all();
-
-    // 1. Upload firmware to DUT: https://probe.rs/docs/library/quickstart/#downloading-to-flash
-    // 2. Upload firmware to client (RP2040)
-    // 3. Start Tests, https://docs.rs/rtt-target/latest/rtt_target/#reading ?
-    // 4. Report Status, use defmt and rtt to read back from the chips?
-    //
 
     let probe_info = probes
         .iter()
         .find(|el| el.serial_number == Some(DEBUG_PROBE_UUID.to_owned()))
         .unwrap();
 
-    dbg!(&probe_info);
+    build_firmware("../fake-peripheral/");
+    flash_firmware(probe_info, "rp2040", FAKE_PERIPHERAL_FIRMWARE_PATH);
 
-    {
-        let probe = probe_info.open().unwrap();
-        let mut session = probe.attach("rp2040", Permissions::default()).unwrap();
+    start_fake_peripheral(probe_info);
+}
 
-        let progress = FlashProgress::new(|event| println!("Event: {:#?}", event));
-        let mut options = DownloadOptions::new();
-        options.progress = Some(progress);
-
-        assert!(Path::new(FAKE_PERIPHERAL_FIRMWARE_PATH).exists());
-
-        download_file_with_options(
-            &mut session,
-            FAKE_PERIPHERAL_FIRMWARE_PATH,
-            Format::Elf,
-            options,
-        )
+fn build_firmware(path: impl AsRef<Path>) {
+    Command::new("cargo")
+        .args(["build", "--release"])
+        .current_dir(path)
+        .output()
         .unwrap();
-    }
+}
 
+fn flash_firmware(
+    probe_info: &DebugProbeInfo,
+    target: impl Into<TargetSelector>,
+    elf: impl AsRef<Path>,
+) {
+    assert!(elf.as_ref().exists(), "Elf path does not exist");
+
+    let probe = probe_info.open().unwrap();
+
+    let mut session = probe.attach(target, Permissions::default()).unwrap();
+
+    download_file(&mut session, elf, Format::Elf).unwrap();
+}
+
+fn start_fake_peripheral(probe_info: &DebugProbeInfo) {
+    // TODO: What here is absolutely necessery
     let probe = probe_info.open().unwrap();
     let mut session = probe.attach("rp2040", Permissions::default()).unwrap();
     let mut core = session.core(0).unwrap();
@@ -61,8 +69,13 @@ fn main() {
 
     let mut rtt = Rtt::attach(&mut core).unwrap();
 
-    let up_channel = rtt.up_channel(0).unwrap();
+    let down_channel = rtt.down_channel(0).unwrap();
 
+    // Send "Start command"
+    down_channel.write(&mut core, &[42]).unwrap();
+
+    // Start reading from the client
+    let up_channel = rtt.up_channel(0).unwrap();
     loop {
         let mut buffer = [0u8; 1024];
         match up_channel.read(&mut core, &mut buffer) {
@@ -77,4 +90,6 @@ fn main() {
     }
 }
 
-fn upload_firmware_peripheral() {}
+fn start_dut(probe_info: &DebugProbeInfo) {
+    todo!()
+}
