d59a66e4d16c9d5e026f9de4413891a16f0716b7
Tamme Dittrich <tamme@tweedegolf.com>
2026-05-15 11:12:21 +0200

    Adapt clippy to run in folders

--- run-in-all.sh
+++ run-in-all.sh
@@ -5,14 +5,14 @@ status=0
 for dir in esp32c6 nRF52 runner shield stm32 test-suite
 do
     pushd $dir > /dev/null
-    echo "--- Running in $dir"
+    echo "--- Running in $dir" 1>&2
     $@
 
     if $@
     then
-        echo "Success"
+        echo "Success" 1>&2
     else
-        echo "Failed"
+        echo "Failed" 1>&2
         status=1
     fi
 
