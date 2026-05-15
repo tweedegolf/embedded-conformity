13d1a8f4a17d1311c449bedb38bd9a1bbc1b082a
Tamme Dittrich <tamme@tweedegolf.com>
2026-05-15 10:37:02 +0200

    Add CI

--- run-in-all.sh
+++ run-in-all.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-# shift
+status=0
 
 for dir in esp32c6 nRF52 runner shield stm32 test-suite
 do
     pushd $dir > /dev/null
     echo "--- Running in $dir"
     $@
+
+    if $@
+    then
+        echo "Success"
+    else
+        echo "Failed"
+        status=1
+    fi
+
     popd > /dev/null
 done
+
+exit $status
